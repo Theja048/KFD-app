8240b24caaabf893e48fa51ce95bd8ab9631f23b
Theja048 <ravitejaa048@gmail.com>
2024-02-26 21:12:22 +0530

    data fetched using live api

--- components/Body.js
+++ components/Body.js
@@ -15,7 +15,7 @@ import resList from "../Utils/DataList"
 			const sdata = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.406498&lng=78.47724389999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
 			const json = await sdata.json();
 			     console.log(json); 
-			 console.log(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+			// console.log(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
 			 setlistofRestarant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
 	 };
   
